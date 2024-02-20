21f1ea3f4bd25c14aaa2e54eba0679f6689ea4d2
Asmik <jasmin_ayr@mail.ru>
2024-02-20 21:23:32 +0100

    Added Sort and Apple task

---
+++ m3-lambda/src/main/java/com/cydeo/ApplePredicate.java
@@ -0,0 +1,7 @@
+package com.cydeo;
+
+@java.lang.FunctionalInterface
+public interface ApplePredicate {
+
+    boolean test(Apple apple);
+}
---
+++ m3-lambda/src/main/java/com/cydeo/BubbleSort.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public class BubbleSort implements Sorting{
+
+    public void sort() {
+        System.out.println("Bubble Sorting");
+    }
+
+}
