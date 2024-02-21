33551a4c86111679a677556519b7fd3ad638ff98
Asmik <jasmin_ayr@mail.ru>
2024-02-21 20:36:43 +0100

    Added Functional Interface Class and Orange and User Tasks

---
+++ m3-lambda/src/main/java/com/cydeo/task/User.java
@@ -0,0 +1,12 @@
+package com.cydeo.task;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Data//Getter Setter ToString
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+}
