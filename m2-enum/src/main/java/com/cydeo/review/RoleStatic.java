f734d588a29f9bcd03d9d7413ce5c306246666b9
Asmik <jasmin_ayr@mail.ru>
2024-02-29 18:15:11 +0100

    adding review tasks for ENUM Module

---
+++ m2-enum/src/main/java/com/cydeo/review/RoleStatic.java
@@ -0,0 +1,9 @@
+package com.cydeo.review;
+
+public class RoleStatic {
+
+    public static final RoleStatic ADMIN = new RoleStatic();
+    public static final RoleStatic MANAGER = new RoleStatic();
+    public static final RoleStatic EMPLOYEE = new RoleStatic();
+
+}
---
+++ m2-enum/src/main/java/com/cydeo/review/User.java
@@ -0,0 +1,16 @@
+package com.cydeo.review;
+
+public class User {
+
+    public String username;
+    public String firstName;
+    public String lastName;
+    private RoleEnum role;
+
+    public void setRole(RoleEnum role){
+        this.role = role;
+    }
+
+
+
+}
