d05ebea30744dfab90728fdb27fa1455dd710edb
Asmik <jasmin_ayr@mail.ru>
2024-03-05 22:49:30 +0100

    Added Java 9 updates

---
+++ m8-new-versions-updates/src/main/java/com/cydeo/java9/PrivateMethodInterface.java
@@ -0,0 +1,12 @@
+package com.cydeo.java9;
+
+import java.time.LocalDate;
+
+public interface PrivateMethodInterface {
+
+    boolean isHoliday(LocalDate date);
+
+    default boolean isBusinessDay(LocalDate date){
+        return true;
+    }
+}
